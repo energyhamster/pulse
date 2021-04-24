fbb17f1b1c3aaec59406a28cd7d026a09b145499
Vlad <vla.opanasenko@gmail.com>
2021-04-24 17:19:05 +0300

    added slick slider

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png"></button>'
+    });
+  });
