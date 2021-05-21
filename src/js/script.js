a0a34b65e4f628de7d00789cfbc6696c9c82508b
Vlad <vla.opanasenko@gmail.com>
2021-05-21 15:08:51 +0300

    added page up button

--- src/js/script.js
+++ src/js/script.js
@@ -95,4 +95,22 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    // Page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    // Smooth scroll
+
+    //    $("a[href^='#']").click(function() {
+    //        const _href = $(this).attr("href");
+    //        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    //        return false;
+    //    });
 });
